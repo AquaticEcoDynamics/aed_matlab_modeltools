682099a220a506ca7d101deb8e32e77cd2412b35
brendan.busch@uwa.edu.au <brendan.busch@uwa.edu.au>
2020-03-11 17:38:23 +0800

    Changes

--- TUFLOWFV/site_processing/swan/BC Load/pre_process_bc_for_load.m
+++ TUFLOWFV/site_processing/swan/BC Load/pre_process_bc_for_load.m
@@ -10,71 +10,71 @@ for i = 3:length(dirlist);
     
     data.(site) = tfv_readBCfile(['Inflows/',dirlist(i).name]);
     
-    load.(site).ML = data.(site).FLOW * (86400/1000);
-    load.(site).L = load.(site).ML * 1e6;
-    load.(site).Date = data.(site).ISOTIME;
+    Load.(site).ML = data.(site).FLOW * (86400/1000);
+    Load.(site).L = Load.(site).ML * 1e6;
+    Load.(site).Date = data.(site).ISOTIME;
     
     
     tnconv = 14/1000;
     tpconv = 31/1000;
     
-    load.(site).TN_mg = ...
-        ((data.(site).AMM * tnconv) .* load.(site).L) + ...
-        ((data.(site).NIT * tnconv) .* load.(site).L) + ...
-        ((data.(site).DON * tnconv) .* load.(site).L) + ...
-        ((data.(site).PON * tnconv) .* load.(site).L) + ...
-        ((data.(site).DONR * tnconv) .* load.(site).L);
+    Load.(site).TN_mg = ...
+        ((data.(site).AMM * tnconv) .* Load.(site).L) + ...
+        ((data.(site).NIT * tnconv) .* Load.(site).L) + ...
+        ((data.(site).DON * tnconv) .* Load.(site).L) + ...
+        ((data.(site).PON * tnconv) .* Load.(site).L) + ...
+        ((data.(site).DONR * tnconv) .* Load.(site).L);
     
-    load.(site).AMM_mg = (data.(site).AMM * tnconv) .* load.(site).L;
-    load.(site).NIT_mg = (data.(site).NIT * tnconv) .* load.(site).L;
-    load.(site).DON_mg = (data.(site).DON * tnconv) .* load.(site).L;
-    load.(site).PON_mg = (data.(site).PON * tnconv) .* load.(site).L;
-    load.(site).DONR_mg = (data.(site).DONR * tnconv) .* load.(site).L;
-    
-    
+    Load.(site).AMM_mg = (data.(site).AMM * tnconv) .* Load.(site).L;
+    Load.(site).NIT_mg = (data.(site).NIT * tnconv) .* Load.(site).L;
+    Load.(site).DON_mg = (data.(site).DON * tnconv) .* Load.(site).L;
+    Load.(site).PON_mg = (data.(site).PON * tnconv) .* Load.(site).L;
+    Load.(site).DONR_mg = (data.(site).DONR * tnconv) .* Load.(site).L;
     
     
     
     
     
-    load.(site).TP_mg = ...
-        ((data.(site).FRP * tpconv) .* load.(site).L) + ...
-        ((data.(site).FRP_ADS * tpconv) .* load.(site).L) + ...
-        ((data.(site).DOP * tpconv) .* load.(site).L) + ...
-        ((data.(site).POP * tpconv) .* load.(site).L) + ...
-        ((data.(site).DOPR * tpconv) .* load.(site).L);
     
     
-    load.(site).FRP_mg = (data.(site).FRP * tpconv) .* load.(site).L;
-    load.(site).FRP_ADS_mg = (data.(site).FRP_ADS * tpconv) .* load.(site).L;
-    load.(site).DOP_mg = (data.(site).DOP * tpconv) .* load.(site).L;
-    load.(site).POP_mg = (data.(site).POP * tpconv) .* load.(site).L;
-    load.(site).DOPR_mg = (data.(site).DOPR * tpconv) .* load.(site).L;
+    Load.(site).TP_mg = ...
+        ((data.(site).FRP * tpconv) .* Load.(site).L) + ...
+        ((data.(site).FRP_ADS * tpconv) .* Load.(site).L) + ...
+        ((data.(site).DOP * tpconv) .* Load.(site).L) + ...
+        ((data.(site).POP * tpconv) .* Load.(site).L) + ...
+        ((data.(site).DOPR * tpconv) .* Load.(site).L);
+    
+    
+    Load.(site).FRP_mg = (data.(site).FRP * tpconv) .* Load.(site).L;
+    Load.(site).FRP_ADS_mg = (data.(site).FRP_ADS * tpconv) .* Load.(site).L;
+    Load.(site).DOP_mg = (data.(site).DOP * tpconv) .* Load.(site).L;
+    Load.(site).POP_mg = (data.(site).POP * tpconv) .* Load.(site).L;
+    Load.(site).DOPR_mg = (data.(site).DOPR * tpconv) .* Load.(site).L;
     
     
     
     
     
-    load.(site).TN_kg = load.(site).TN_mg /1e-6;
-    load.(site).TP_kg = load.(site).TP_mg /1e-6;
+    Load.(site).TN_kg = Load.(site).TN_mg /1e-6;
+    Load.(site).TP_kg = Load.(site).TP_mg /1e-6;
     
-    load.(site).AMM_kg = load.(site).AMM_mg /1e-6;
-    load.(site).NIT_kg = load.(site).NIT_mg /1e-6;
-    load.(site).DON_kg = load.(site).DON_mg /1e-6;
-    load.(site).PON_kg = load.(site).PON_mg /1e-6;
-    load.(site).DONR_kg = load.(site).DONR_mg /1e-6;
+    Load.(site).AMM_kg = Load.(site).AMM_mg /1e-6;
+    Load.(site).NIT_kg = Load.(site).NIT_mg /1e-6;
+    Load.(site).DON_kg = Load.(site).DON_mg /1e-6;
+    Load.(site).PON_kg = Load.(site).PON_mg /1e-6;
+    Load.(site).DONR_kg = Load.(site).DONR_mg /1e-6;
     
-    load.(site).FRP_kg = load.(site).FRP_mg /1e-6;
-    load.(site).FRP_ADS_kg = load.(site).FRP_ADS_mg /1e-6;
-    load.(site).DOP_kg = load.(site).DOP_mg /1e-6;
-    load.(site).POP_kg = load.(site).POP_mg /1e-6;
-    load.(site).DOPR_kg = load.(site).DOPR_mg /1e-6;
+    Load.(site).FRP_kg = Load.(site).FRP_mg /1e-6;
+    Load.(site).FRP_ADS_kg = Load.(site).FRP_ADS_mg /1e-6;
+    Load.(site).DOP_kg = Load.(site).DOP_mg /1e-6;
+    Load.(site).POP_kg = Load.(site).POP_mg /1e-6;
+    Load.(site).DOPR_kg = Load.(site).DOPR_mg /1e-6;
     
 end
 
-save load.mat load -mat;
+save Load.mat Load -mat;
 
-sites = fieldnames(load);
+sites = fieldnames(Load);
 
 fid = fopen('Flow.csv','wt');
 
@@ -85,12 +85,12 @@ for i = 1:length(sites)
 end
 fprintf(fid,'\n');
 
-for j = 1:length(load.(sites{1}).Date);
-    fprintf(fid,'%s,',datestr(load.(sites{1}).Date(j),'dd/mm/yyyy'));
+for j = 1:length(Load.(sites{1}).Date);
+    fprintf(fid,'%s,',datestr(Load.(sites{1}).Date(j),'dd/mm/yyyy'));
     
     for i = 1:length(sites)
-        if load.(sites{i}).Date(j) == load.(sites{1}).Date(j)
-            fprintf(fid,'%4.10f,',load.(sites{i}).ML(j));
+        if Load.(sites{i}).Date(j) == Load.(sites{1}).Date(j)
+            fprintf(fid,'%4.10f,',Load.(sites{i}).ML(j));
         else
             stop;
         end
@@ -112,12 +112,12 @@ for i = 1:length(sites)
 end
 fprintf(fid,'\n');
 
-for j = 1:length(load.(sites{1}).Date);
-    fprintf(fid,'%s,',datestr(load.(sites{1}).Date(j),'dd/mm/yyyy'));
+for j = 1:length(Load.(sites{1}).Date);
+    fprintf(fid,'%s,',datestr(Load.(sites{1}).Date(j),'dd/mm/yyyy'));
     
     for i = 1:length(sites)
-        if load.(sites{i}).Date(j) == load.(sites{1}).Date(j)
-            fprintf(fid,'%4.10f,',load.(sites{i}).TN_kg(j));
+        if Load.(sites{i}).Date(j) == Load.(sites{1}).Date(j)
+            fprintf(fid,'%4.10f,',Load.(sites{i}).TN_kg(j));
         else
             stop;
         end
@@ -138,12 +138,12 @@ for i = 1:length(sites)
 end
 fprintf(fid,'\n');
 
-for j = 1:length(load.(sites{1}).Date);
-    fprintf(fid,'%s,',datestr(load.(sites{1}).Date(j),'dd/mm/yyyy'));
+for j = 1:length(Load.(sites{1}).Date);
+    fprintf(fid,'%s,',datestr(Load.(sites{1}).Date(j),'dd/mm/yyyy'));
     
     for i = 1:length(sites)
-        if load.(sites{i}).Date(j) == load.(sites{1}).Date(j)
-            fprintf(fid,'%4.10f,',load.(sites{i}).TP_kg(j));
+        if Load.(sites{i}).Date(j) == Load.(sites{1}).Date(j)
+            fprintf(fid,'%4.10f,',Load.(sites{i}).TP_kg(j));
         else
             stop;
         end

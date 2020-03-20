9844ebcea6608ad4f500417e4cc4f623c044c7ae
brendan-busch <brendan.busch@uwa.edu.au>
2020-03-20 12:30:42 +0800

    my changes

--- TUFLOWFV/site_processing/swan/BC Load/pre_process_bc_for_load.m
+++ TUFLOWFV/site_processing/swan/BC Load/pre_process_bc_for_load.m
@@ -22,8 +22,8 @@ for i = 3:length(dirlist);
         ((data.(site).AMM * tnconv) .* Load.(site).L) + ...
         ((data.(site).NIT * tnconv) .* Load.(site).L) + ...
         ((data.(site).DON * tnconv) .* Load.(site).L) + ...
-        ((data.(site).PON * tnconv) .* Load.(site).L) + ...
-        ((data.(site).DONR * tnconv) .* Load.(site).L);
+        ((data.(site).PON * tnconv) .* Load.(site).L);% + ...
+        %((data.(site).DONR * tnconv) .* Load.(site).L);
     
     Load.(site).AMM_mg = (data.(site).AMM * tnconv) .* Load.(site).L;
     Load.(site).NIT_mg = (data.(site).NIT * tnconv) .* Load.(site).L;
@@ -55,20 +55,20 @@ for i = 3:length(dirlist);
     
     
     
-    Load.(site).TN_kg = Load.(site).TN_mg /1e-6;
-    Load.(site).TP_kg = Load.(site).TP_mg /1e-6;
+    Load.(site).TN_kg = Load.(site).TN_mg *1e-6;
+    Load.(site).TP_kg = Load.(site).TP_mg *1e-6;
     
-    Load.(site).AMM_kg = Load.(site).AMM_mg /1e-6;
-    Load.(site).NIT_kg = Load.(site).NIT_mg /1e-6;
-    Load.(site).DON_kg = Load.(site).DON_mg /1e-6;
-    Load.(site).PON_kg = Load.(site).PON_mg /1e-6;
-    Load.(site).DONR_kg = Load.(site).DONR_mg /1e-6;
+    Load.(site).AMM_kg = Load.(site).AMM_mg *1e-6;
+    Load.(site).NIT_kg = Load.(site).NIT_mg *1e-6;
+    Load.(site).DON_kg = Load.(site).DON_mg *1e-6;
+    Load.(site).PON_kg = Load.(site).PON_mg *1e-6;
+    Load.(site).DONR_kg = Load.(site).DONR_mg *1e-6;
     
-    Load.(site).FRP_kg = Load.(site).FRP_mg /1e-6;
-    Load.(site).FRP_ADS_kg = Load.(site).FRP_ADS_mg /1e-6;
-    Load.(site).DOP_kg = Load.(site).DOP_mg /1e-6;
-    Load.(site).POP_kg = Load.(site).POP_mg /1e-6;
-    Load.(site).DOPR_kg = Load.(site).DOPR_mg /1e-6;
+    Load.(site).FRP_kg = Load.(site).FRP_mg *1e-6;
+    Load.(site).FRP_ADS_kg = Load.(site).FRP_ADS_mg *1e-6;
+    Load.(site).DOP_kg = Load.(site).DOP_mg *1e-6;
+    Load.(site).POP_kg = Load.(site).POP_mg *1e-6;
+    Load.(site).DOPR_kg = Load.(site).DOPR_mg *1e-6;
     
 end
 
